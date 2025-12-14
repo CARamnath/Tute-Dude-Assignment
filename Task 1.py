eb38b1010e35a13aba00449d4efbf2e72fd0bc80
CARamnath <cashanbhag10@gmail.com>
2025-12-14 15:06:43 +0530

    Add files via upload
    Assignment 1

---
+++ Task 1.py
@@ -0,0 +1,8 @@
+#Task 1: Perform Basic Mathematical Operations
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+
